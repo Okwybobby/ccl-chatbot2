c6d4b82bd3e5b5b5c94015f72be7f58adcf48f50
Okwybobby <59561801+Okwybobby@users.noreply.github.com>
2024-04-17 08:23:53 +0100

    Fixed select issue

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,23 +1,33 @@
 'use client'
 import useSWR from "swr";
-import Select from "react-select/creatable/dist/react-select-creatable.cjs";
-// import Select from "react-select";
+// import Select from "react-select/creatable/dist/react-select-creatable.cjs";
+import Select from "react-select";
 
 
 const fetchModels = () => fetch("/api/getEngines").then((res) => res.json())
 function ModelSelection() {
   const {data: models, isLoading } = useSWR("models", fetchModels)
+  const {data: model, mutate: setModel } = useSWR("model", {
+
+    fallbackData: 'const model = "gpt-3.5-turbo"'
+  })
+  
 
   return (
     <div>
-      <Select className="mt-2"
-      // defaultValue={model}
+      <Select 
+      className="mt-2"
+      options={models?.modelOptions}
+      defaultValue={model}
+      placeholder={model}
       isSearchable
       isLoading={isLoading}
       menuPosition="fixed"
       classNames={{
         control: (state) => "bg-[#434654] border-[#434654]"
       }}
+      onChange={(e) => setModel(e.value)}
+
       />
     </div>
   )
